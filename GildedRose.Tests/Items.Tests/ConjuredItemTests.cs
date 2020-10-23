cac3eda73753d016773c25d5d5ddb9a41474f38a
Audrius Liuberskis <audriusliub@gmail.com>
2020-10-23 14:12:38 +0300

    Added a class for conjured items, and tests for it

---
+++ GildedRose.Tests/Items.Tests/ConjuredItemTests.cs
@@ -0,0 +1,61 @@
+﻿using GildedRose.Items;
+using Shouldly;
+using Xunit;
+
+namespace GildedRose.Tests.Items.Tests
+{
+    public class ConjuredItemTests
+    {
+        [Theory]
+        [InlineData("Conjured Mana Cake", 3, 6)]
+        public void Ctor_WhenCalled_ShouldSetUpPropertiesCorrectly(string name, int sellIn, int quality)
+        {
+            // Arrange, Act
+            var item = new ConjuredItem(name, sellIn, quality);
+            // Assert
+            item.Name.ShouldBe(name);
+            item.SellIn.ShouldBe(sellIn);
+            item.Quality.ShouldBe(quality);
+            item.DegradationRate.ShouldBe(2);
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void Age_WhenSellInIsPositive_ShouldDecreaseQualityByTwo(int sellIn)
+        {
+            // Arrange
+            var initialQuality = 10;
+            var item = new ConjuredItem("", sellIn, initialQuality);
+            // Act
+            item.Age();
+            // Assert
+            item.Quality.ShouldBe(initialQuality - 2);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-2)]
+        public void Age_WhenSellInIsNegativeOrZero_ShouldDecreaseQualityByFour(int sellIn)
+        {
+            // Arrange
+            var initialQuality = 10;
+            var item = new ConjuredItem("", sellIn, initialQuality);
+            // Act
+            item.Age();
+            // Assert
+            item.Quality.ShouldBe(initialQuality - 4);
+        }
+
+        [Fact]
+        public void Age_WhenQualityWouldFallBelowZero_ShouldSetQualityToZero()
+        {
+            // Arrange
+            var item = new ConjuredItem("", -2, 2);
+            // Act
+            item.Age();
+            // Assert
+            item.Quality.ShouldBe(0);
+        }
+    }
+}
---
+++ GildedRose/Items/ConjuredItem.cs
@@ -0,0 +1,16 @@
+﻿namespace GildedRose.Items
+{
+    class ConjuredItem : GenericItem
+    {
+        public ConjuredItem(string name, int sellIn, int quality) : base(name, sellIn, quality)
+        {
+            DegradationRate = 2;
+            // NOTE: It wasn't quite clear to me what the requirements for this type of item were.
+            // At first, I thought that any item could be conjured, and planned to use a decorator
+            // But the sample just gives an independent conjured item, and i.e. a conjured backstage
+            // pass wouldn't really make sense. In a real scenario, I'd ask the client to clarify,
+            // but since that's not an option during this exercise, I assumed the different types of 
+            // items do not overlap (no conjured aged bries, no conjured sulfuras etc)
+        }
+    }
+}
